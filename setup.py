d4448417af4baaa4ae250458d750a5bc12895581
Sl4yM4yd4y <attilaotvos8@gmail.com>
2024-10-19 14:13:49 +0200

    formatting package

--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'ros_yolo_model'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
+    py_modules=[
+        'detector'
+    ],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/yolo.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ajr',
-    maintainer_email='attilaotvos8@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='M4yd4y',
+    maintainer_email='attilaotvos8@g.com',
+    description='YOLO object detection using ROS2',
+    license='GNU GENERAL PUBLIC LICENSE 3.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'detector = ros_yolo_model.detector:main'
+            'detector = detector:main',
         ],
     },
 )
