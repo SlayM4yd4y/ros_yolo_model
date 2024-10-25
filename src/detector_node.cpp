cc1d0f4f430e6382d9a6bbab9b1b25827db3c48e
Sl4yM4yd4y <attilaotvos8@gmail.com>
2024-10-25 12:34:37 +0200

    removed redefinition of class YOLODetector

--- src/detector_node.cpp
+++ src/detector_node.cpp
@@ -1,43 +1,36 @@
 #include "detector.hpp"
 #include <std_msgs/msg/string.hpp>
 
-class YOLODetector : public rclcpp::Node {
-private:
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr object_pub_;
+YOLODetector::YOLODetector() : Node("yolo_detector") {
+    RCLCPP_INFO(this->get_logger(), "YOLO Detector Node elindult.");
+    image_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
+        "camera/image", 10, std::bind(&YOLODetector::detect_callback, this, std::placeholders::_1));
 
-public:
-    YOLODetector() : Node("yolo_detector") {
-        RCLCPP_INFO(this->get_logger(), "YOLO Detector Node elindult.");
-        image_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
-            "camera/image", 10, std::bind(&YOLODetector::detect_callback, this, std::placeholders::_1));
+    object_pub_ = this->create_publisher<std_msgs::msg::String>("object_detection", 10);
+}
 
-        object_pub_ = this->create_publisher<std_msgs::msg::String>("object_detection", 10);
+void YOLODetector::detect_callback(const sensor_msgs::msg::Image::SharedPtr msg) {
+    cv_bridge::CvImagePtr cv_ptr;
+    try {
+        cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
+    } catch (cv_bridge::Exception &e) {
+        RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+        return;
     }
 
-    void detect_callback(const sensor_msgs::msg::Image::SharedPtr msg) {
-        cv_bridge::CvImagePtr cv_ptr;
-        try {
-            cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
-        } catch (cv_bridge::Exception &e) {
-            RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
-            return;
-        }
+    cv::Mat frame = cv_ptr->image;
 
-        cv::Mat frame = cv_ptr->image;
+    // YOLO modell használata a detektáláshoz (TODO: itt kéne a detektálást implementálni)
+    std_msgs::msg::String result_msg;
+    result_msg.data = "Object detected"; // Ezt cseréld ki a valódi detektálás eredményére
 
-        // YOLO modell használata a detektáláshoz (TODO: itt kéne a detektálást implementálni)
-        std_msgs::msg::String result_msg;
-        result_msg.data = "Object detected"; // Ezt cseréld ki a valódi detektálás eredményére
+    // Publikálás az object_detection topicra
+    object_pub_->publish(result_msg);
 
-        // Publikálás az object_detection topicra
-        object_pub_->publish(result_msg);
-
-        // Eredmények megjelenítése OpenCV ablakban
-        cv::imshow("YOLO Detection", frame);
-        cv::waitKey(1);
-    }
-};
+    // Eredmények megjelenítése OpenCV ablakban
+    cv::imshow("YOLO Detection", frame);
+    cv::waitKey(1);
+}
 
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
