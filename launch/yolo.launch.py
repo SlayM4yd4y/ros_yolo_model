b0ac6ac581a5b13dd008824712e758d40300c506
Sl4yM4yd4y <attilaotvos8@gmail.com>
2024-10-30 12:43:43 +0100

    Refactor yolo.launch.py TODO: separate training and card generation nodes

--- launch/yolo.launch.py
+++ launch/yolo.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+#TODO: megcsinalni ugy hogy ez csak a tanitas inditasahoz kelljen paramerekkel, a card gen az siman run-os maradhat 
 def generate_launch_description():
     return LaunchDescription([
         Node(
