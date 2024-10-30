2f70afde359715ba397a29d1cb24451e036bb851
Sl4yM4yd4y <attilaotvos8@gmail.com>
2024-10-30 20:39:47 +0100

    Refactor detector: remove YOLODetector class and introduce DetectorNode class for improved structure, rename added new funcs

---
+++ headers/detector_node.hpp
@@ -0,0 +1,33 @@
+#ifndef DETECTOR_NODE_HPP
+#define DETECTOR_NODE_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include <string>
+
+class DetectorNode : public rclcpp::Node {
+public:
+    DetectorNode();
+    
+
+private:
+    void executeDetectionCommand(const std::string& source);
+    void detectLiveCamera(int camera_id);
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr object_pub_;
+    std::string weights_path_;
+    std::string data_path_;
+    std::string source_type_;  
+    std::string video_path_;   
+    std::string image_path_;
+    std::string save_dir_;    
+    int camera_id_;             
+    float conf_thres_;
+    float iou_thres_;
+    bool view_img_;
+};
+
+#endif  // DETECTOR_NODE_HPP
