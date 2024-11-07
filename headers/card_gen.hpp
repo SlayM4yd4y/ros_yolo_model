4403db79d23470dff7297110503443dd8b309075
gera0719 <gergonovak503@gmail.com>
2024-11-07 12:56:12 +0100

    Refactor card generation to use vectors for dynamic data handling and to create dummy cards for all variations of the given data

--- headers/card_gen.hpp
+++ headers/card_gen.hpp
@@ -8,14 +8,23 @@
 #include <unistd.h>
 #include "ament_index_cpp/get_package_share_directory.hpp"
 #include <iostream>
+#include <vector>
 
 class CardGenerator : public rclcpp::Node
 {
 private:
     //static int counter;
+    std::vector<std::string> names;
+    std::vector<std::string> nCodes;
+    std::vector<std::string> ids;
+    std::vector<std::string> images;
+    std::vector<std::string> barcodes;
+    std::vector<std::string> cardIds;
+    std::vector<std::string> cardTemplates;
+
 public:
     CardGenerator();
-    void generate_card(const std::string& output_dir, int count);
+    void generate_card(const std::string& output_dir);
 };
 
 std::string package_path();
--- src/card_gen_node.cpp
+++ src/card_gen_node.cpp
@@ -8,9 +8,17 @@ int CardGenerator::counter = 1;
 CardGenerator::CardGenerator() : Node("card_generator")
 {
     RCLCPP_INFO(this->get_logger(), "Card Generator Node Started");
+    //initialize the vectors with the names, ncodes, id, images, barcodes and cardId
+    names = {"John Doe", "Jane Doe", "John Smith"};
+    nCodes = {"A1B2C3", "D4E5F6", "G7H8I9"};
+    ids = {"56981237", "66982238", "45981239"};
+    cardIds = {"15268647", "25268648", "85268649"};
+    images = {package_path()+"/img/card_gen/portrait1.png", package_path()+"/img/card_gen/portrait2.png", package_path()+"/img/card_gen/portrait3.png"};
+    barcodes = {package_path()+"/img/card_gen/rsz_barcode1.png", package_path()+"/img/card_gen/rsz_barcode2.png", package_path()+"/img/card_gen/rsz_barcode3.png"};
+    cardTemplates = {package_path()+"/img/card_gen/student_c.png", package_path()+"/img/card_gen/employee_c.png"};
 }
 
-void CardGenerator::generate_card(const std::string& output_dir, int count)
+void CardGenerator::generate_card(const std::string& output_dir)
 {
     RCLCPP_INFO(this->get_logger(), "Generating card...");
 
@@ -21,43 +29,81 @@ void CardGenerator::generate_card(const std::string& output_dir, int count)
     }
 
     // generating the dummy card
-    cv::Mat card(359, 553, CV_8UC3, cv::Scalar(255, 255, 255));
+    int counter = 0;
 
-    auto ft2 = cv::freetype::createFreeType2();
-    ft2->loadFontData(package_path() + "/fonts/Montserrat/Montserrat-VariableFont_wght.ttf", 0);
 
-    cv::Mat card_template = cv::imread(package_path() + "/img/card_gen/card_template.png");
-    if (card_template.empty()) {
-        RCLCPP_ERROR(this->get_logger(), "Failed to load card template image");
-        return;
+
+
+    for (int i = 0; i < static_cast<int>(cardTemplates.size()); i++){
+        for (int ii = 0; ii < static_cast<int>(names.size()); ii++){
+            for (int iii = 0; iii < static_cast<int>(cardIds.size()); iii++){
+                for (int iv = 0; iv < static_cast<int>(images.size()); iv++){
+                    for (int v = 0; v < static_cast<int>(barcodes.size()); v++){
+                        for (int vi = 0; vi < static_cast<int>(ids.size()); vi++){
+
+                            
+
+                            cv::Mat card(359, 553, CV_8UC3, cv::Scalar(255, 255, 255));
+                            auto ft2 = cv::freetype::createFreeType2();
+                            ft2->loadFontData(package_path() + "/fonts/Montserrat/Montserrat-VariableFont_wght.ttf", 0);
+
+                            cv::Mat card_template = cv::imread(cardTemplates[i]);
+                            if (cardTemplates.empty()){
+                                RCLCPP_ERROR(this->get_logger(), "Failed to load template image");
+                                return;
+                            }
+                            cv::Mat image = cv::imread(images[iv]);
+                            if (images.empty()){
+                                RCLCPP_ERROR(this->get_logger(), "Failed to load an image from the corresponding vector");
+                                return;
+                            }
+                            cv::Mat barcode = cv::imread(barcodes[v]);
+                            if (barcodes.empty()){
+                                RCLCPP_ERROR(this->get_logger(), "Failed to load a barcode from the corresponding vector");
+                                return;
+                            }
+                            if (cardTemplates[i].find("student") != std::string::npos){
+
+                                cv::Rect ct(cv::Point(0, 0), card_template.size());
+                                cv::Rect bc(cv::Point(41, 29), barcode.size());
+                                cv::Rect pt(cv::Point(382, 122), image.size());
+                                card_template.copyTo(card(ct));
+                                barcode.copyTo(card(bc));
+                                image.copyTo(card(pt));
+
+                                ft2->putText(card, names[ii], cv::Point(61, 297), 15, cv::Scalar(0, 0, 0), 1, cv::LINE_AA, false);
+                                ft2->putText(card, "H" + cardIds[iii], cv::Point(158, 230), 15, cv::Scalar(0, 0, 0), 1, cv::LINE_AA, false);
+                                ft2->putText(card, nCodes[vi], cv::Point(157, 260), 15, cv::Scalar(0, 0, 0), 1, cv::LINE_AA, false);
+
+                                
+                            }else if (cardTemplates[i].find("employee") != std::string::npos){
+                                cv::Rect ct(cv::Point(0, 0), card_template.size());
+                                cv::Rect bc(cv::Point(49, 22), barcode.size());
+                                cv::Rect pt(cv::Point(386, 123), image.size());
+                                card_template.copyTo(card(ct));
+                                barcode.copyTo(card(bc));
+                                image.copyTo(card(pt));
+
+                                ft2->putText(card, names[ii], cv::Point(65, 297), 15, cv::Scalar(0, 0, 0), 1, cv::LINE_AA, false);
+                                ft2->putText(card, "A" + cardIds[iii], cv::Point(165, 230), 15, cv::Scalar(0, 0, 0), 1, cv::LINE_AA, false);
+                                ft2->putText(card, ids[vi], cv::Point(165, 260), 15, cv::Scalar(0, 0, 0), 1, cv::LINE_AA, false);
+                                
+                                
+                            }
+                            // variable with the card filename
+                            std::string card_filename = output_dir + "/card" + std::to_string(counter) + ".png";
+
+                            // saving the card to the output directory
+                            cv::imwrite(card_filename, card);
+                            RCLCPP_INFO(this->get_logger(), "Card saved to %s", output_dir.c_str());
+                            ++counter;
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    cv::Mat barcode = cv::imread(package_path() + "/img/card_gen/rsz_barcode1.png");
-    if (barcode.empty()) {
-        RCLCPP_ERROR(this->get_logger(), "Failed to load barcode image");
-        return;
-    }
-
-    cv::Mat portrait = cv::imread(package_path() + "/img/card_gen/portrait1.png");
-    if (portrait.empty()) {
-        RCLCPP_ERROR(this->get_logger(), "Failed to load portrait image");
-        return;
-    }
-    
-    cv::Rect ct(cv::Point(0, 0), card_template.size());
-    cv::Rect bc(cv::Point(41, 29), barcode.size());
-    cv::Rect pt(cv::Point(382, 122), portrait.size());
-
-    card_template.copyTo(card(ct));
-    barcode.copyTo(card(bc));
-    portrait.copyTo(card(pt));
-
-    // variable with the card filename
-    std::string card_filename = output_dir + "/card" + std::to_string(count) + ".png";
-
-    // saving the card to the output directory
-    cv::imwrite(card_filename, card);
-    RCLCPP_INFO(this->get_logger(), "Card saved to %s", output_dir.c_str());
 }
 
 std::string package_path()
@@ -75,9 +121,7 @@ int main(int argc, char **argv)
     auto card_gen = std::make_shared<CardGenerator>();
 
     //to be made with static variable
-    for (int i = 0; i < 5; i++){
-        card_gen->generate_card(package_path() + "/generated_cards", i);
-    }
+    card_gen->generate_card(package_path() + "/generated_cards");
 
     rclcpp::spin_some(card_gen);
     rclcpp::shutdown();
